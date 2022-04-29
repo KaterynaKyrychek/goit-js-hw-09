a75ce04fcb323aab0f63569dbf9bec98ad843305
KaterynaKyrychek <90039728+KaterynaKyrychek@users.noreply.github.com>
2022-04-29 21:10:08 +0300

    v1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,14 @@
+import getRandomHexColor from '';
+const btnStart = document.querySelector('button[data-start]');
+const btnFinish = document.querySelector('button[data-finish]');
+const bodyColor = document.querySelector('body');
 
+let timerId = null;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+btnStart.addEventListener("click", () => {
+
+})
