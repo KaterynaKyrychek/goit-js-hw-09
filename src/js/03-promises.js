6a75d17c4be8c707ff83e7b4a39f766b8872e1fc
KaterynaKyrychek <90039728+KaterynaKyrychek@users.noreply.github.com>
2022-05-01 16:36:45 +0300

    v3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  delay: document.querySelector('input[name="delay"]'),
+  step: document.querySelector('input[name="step"]'),
+  amount: document.querySelector('input[name="amount"]'),
+  };
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+    if (shouldResolve) {
+      resolve({position: position, delay: delay});
   } else {
-    // Reject
+    reject({position:position, delay: delay});
+  }  
+    }, delay);
+  });  
+}
+
+function onSubmit(event) {
+  event.preventDefault();
+  const incomingValues = {
+    delay: parseInt(refs.delay.value),
+    step: parseInt(refs.step.value),
+    amount: parseInt(refs.amount.value),
+  };
+  creareNewPromise(incomingValues);
+}
+
+function creareNewPromise({delay, step, amount}) {
+  let totalDelay = delay;
+  for (let i = 1; i<= amount; i += 1) {
+    createPromise(i, totalDelay).then(({position, delay}) => {
+      Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({position, delay}) => {
+      Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+    });
+    totalDelay += step;
   }
 }
